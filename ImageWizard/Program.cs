f0b723418bf1f89d191e9ad3289d057e32b90d55
Rhoxolan <pba17mail@gmail.com>
2024-01-27 22:08:18 +0200

    Add some configurations for future auth

--- ImageWizard/Program.cs
+++ ImageWizard/Program.cs
@@ -3,40 +3,62 @@ using ImageWizard.Services.ImagesServices.GetImageUrlService;
 using ImageWizard.Services.ImagesServices.ImagesFileWorkerService;
 using ImageWizard.Services.ImagesServices.SaveImageService;
 using ImageWizard.Services.ImagesServices.UploadFromUrlImageService;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ImagesContext>(opt
-    => opt.UseSqlServer(builder.Configuration.GetConnectionString("ImagesDB")));
+	=> opt.UseSqlServer(builder.Configuration.GetConnectionString("ImagesDB")));
 
 builder.Services.AddControllers();
 
 builder.Services.AddCors(opt =>
-    opt.AddDefaultPolicy(builder =>
-    builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
+	opt.AddDefaultPolicy(builder =>
+	builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
 	.AddEntityFrameworkStores<ImagesContext>();
 
+builder.Services.AddAuthentication(opt =>
+{
+	opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+	opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(opt =>
+{
+	opt.TokenValidationParameters = new TokenValidationParameters
+	{
+		ValidateIssuer = true,
+		ValidateAudience = true,
+		ValidateLifetime = true,
+		ValidateIssuerSigningKey = true,
+		ValidIssuer = builder.Configuration["Jwt:Issuer"],
+		ValidAudience = builder.Configuration["Jwt:Audience"],
+		IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SecretKey"]!))
+	};
+});
+
 builder.Services.AddEndpointsApiExplorer()
-    .AddHttpClient()
-    .AddSwaggerGen()
-    .AddTransient<IUploadFromUrlImageService, UploadFromUrlImageService>()
-    .AddSingleton<IImagesFileWorkerService, ImagesFileWorkerService>()
-    .AddTransient<IImageService, ImageService>()
-    .AddTransient<IGetImageUrlService, GetImageUrlService>();
+	.AddHttpClient()
+	.AddSwaggerGen()
+	.AddTransient<IUploadFromUrlImageService, UploadFromUrlImageService>()
+	.AddSingleton<IImagesFileWorkerService, ImagesFileWorkerService>()
+	.AddTransient<IImageService, ImageService>()
+	.AddTransient<IGetImageUrlService, GetImageUrlService>();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
 app.UseCors();
 
