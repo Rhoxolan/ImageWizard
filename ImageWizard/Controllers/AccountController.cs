5f473f3724a756fb30f39b659b1b3dc60a8bc795
Rhoxolan <pba17mail@gmail.com>
2024-01-28 14:48:16 +0200

    Added the account controller

---
+++ ImageWizard/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageWizard.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AccountController : ControllerBase
+	{
+		private readonly UserManager<IdentityUser> _userManager;
+
+		public AccountController(UserManager<IdentityUser> userManager)
+		{
+			_userManager = userManager;
+		}
+
+		[HttpPost("register")]
+		public async Task<IActionResult> Register()
+		{
+			throw new NotImplementedException();
+		}
+
+		[HttpPost("login")]
+		public async Task<IActionResult> Login()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
