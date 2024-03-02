55307749abf0cbdf1ba1a382e6d5cfff92e832c9
Rhoxolan <pba17mail@gmail.com>
2024-03-03 00:07:31 +0200

    Modified the auth token check middelwere. Added support for get image method.

--- ImageWizard/Program.cs
+++ ImageWizard/Program.cs
@@ -68,16 +68,22 @@ app.UseAuthentication();
 
 app.Use(async (context, next) =>
 {
-	if(context.Request.Path.Value == "/api/images" && context.Request.Method.ToUpper() == "POST"
-	&& !string.IsNullOrEmpty(context.Request.Headers["Authorization"]))
+	bool isPostImages = context.Request.Path.Value == "/api/images" && context.Request.Method.ToUpper() == "POST";
+	bool isGetImages = context.Request.Path.Value!.StartsWith("/api/images/") && context.Request.Method.ToUpper() == "GET";
+	bool isContainsAuthHeader = !string.IsNullOrEmpty(context.Request.Headers["Authorization"]);
+
+	if (isContainsAuthHeader && (isPostImages || isGetImages))
 	{
 		var authResult = await context.AuthenticateAsync(JwtBearerDefaults.AuthenticationScheme);
 		if (!authResult.Succeeded)
 		{
 			context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+			context.Response.Headers.AccessControlAllowOrigin = "*";
+			context.Response.Headers.ContentType = "application/problem+json; charset=utf-8";
 			return;
 		}
 	}
+
 	await next();
 });
 
